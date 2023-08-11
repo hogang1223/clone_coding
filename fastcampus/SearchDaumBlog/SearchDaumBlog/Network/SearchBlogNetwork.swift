7da322001cc7d669577a003540b5c9058f473150
hogang <hyogangster1@gmail.com>
2023-08-11 22:11:28 +0900

    fastcampus - rxswift - SearchDaumBlog - call api

---
+++ fastcampus/SearchDaumBlog/SearchDaumBlog/Network/SearchBlogNetwork.swift
@@ -0,0 +1,45 @@
+//
+//  SearchBlogNetwork.swift
+//  SearchDaumBlog
+//
+//  Created by hogang on 2023/08/11.
+//
+
+import Foundation
+import RxSwift
+
+enum SearchNetworkError: Error {
+    case invaildURL
+    case invalidJSON
+    case networkError
+}
+
+class SearchBlogNetwork {
+    private let session: URLSession
+    let api = SearchBlogAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func searchBlog(query: String) -> Single<Result<DKBlog, SearchNetworkError>> {
+        guard let url = api.searchBlog(query: query).url else { return .just(.failure(.invaildURL)) }
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let blogData = try JSONDecoder().decode(DKBlog.self, from: data)
+                    return .success(blogData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                    .just(.failure(.networkError))
+            }
+            .asSingle()
+    }
+}
