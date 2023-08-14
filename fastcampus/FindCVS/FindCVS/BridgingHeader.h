83615af17ff28c6a068ccb8e6d752176cae90340
hogang <hyogangster1@gmail.com>
2023-08-14 19:38:27 +0900

    kakaoMap Api - rxswift - mvvm - ui그리기

---
+++ fastcampus/FindCVS/FindCVS/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  FindCVS
+//
+//  Created by hogang on 2023/08/14.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+
+#endif /* BridgingHeader_h */
---
+++ fastcampus/FindCVS/FindCVS/Entities/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  FindCVS
+//
+//  Created by hogang on 2023/08/14.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
